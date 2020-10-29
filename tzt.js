7eab7a079382309ae9caaaf317dc71533d548bcc
romovs <romovs@gmail.com>
2020-10-29 14:56:00 +0200

    initial commit

---
+++ tzt.js
@@ -0,0 +1,10 @@
+web.transaction('INitialize Web')
+
+web.init();
+
+web.transaction('Open Web site')
+
+web.open('http://www.wikipedia.org');
+web.setTimeout(1000);
+web.assertTitle('dssssssssss');
+
